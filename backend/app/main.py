95881aec6190862b42d21af7ada32310003daf65
Ratnadeep-2007 <ratnadeepp2007@gmail.com>
2026-05-04 22:32:46 +0530

    Fix CORS for deployed frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,6 +24,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://spider-smart-ims-001.web.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
